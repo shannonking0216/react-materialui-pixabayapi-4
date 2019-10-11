503fef34d70faddde711defac962a5115fc2410e
Shannon King <kingshannon01@gmail.com>
2019-10-11 16:25:21 -0700

    creating gridtiles for image results

--- pixabay_image_finder/src/components/image-results/ImageResults.js
+++ pixabay_image_finder/src/components/image-results/ImageResults.js
@@ -1,11 +1,36 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { GridList, GridTile } from "material-ui/GridList";
+import IconButton from "material-ui/IconButton";
+import ZoomIn from "material-ui/svg-icons/action/zoom-in";
+import Dialog from "material-ui/Dialog";
+import FlatButton from "material-ui/FlatButton";
+import GridTile from "material-ui/GridList/GridTile";
 
-export default class ImageResults extends Component {
-    render() {
-        return (
-            <div>
+class ImageResults extends Component {
+  render() {
+    let imageListContent;
+    const { images } = this.props;
+
+    if (images) {
+      imageListContent = (
+        <GridList cols={3}>
+          {images.map(img => (
+            <GridTile>
                 
-            </div>
-        )
+            </GridTile>
+          ))}
+        </GridList>
+      );
+    } else {
+      imageListContent = null;
     }
+    return <div></div>;
+  }
 }
+
+ImageResults.propTypes = {
+  images: PropTypes.array.isRequired
+};
+
+export default ImageResults;
