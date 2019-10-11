f037f6a42b04ffc4bbede0bb87a63473763b7dd1
Shannon King <kingshannon01@gmail.com>
2019-10-11 15:52:38 -0700

    image-results

--- pixabay_image_finder/src/components/search/Search.js
+++ pixabay_image_finder/src/components/search/Search.js
@@ -3,6 +3,7 @@ import TextField from "material-ui/TextField";
 import SelectField from "material-ui/SelectField";
 import MenuItem from "material-ui/MenuItem";
 import axios from "axios";
+import ImageResults from "../image-results/ImageResults";
 
 class Search extends Component {
   state = {
@@ -17,16 +18,14 @@ class Search extends Component {
     this.setState({ [e.target.name]: e.target.value }, () => {
       axios
         .get(
-          `${this.state.apiUrl}/?key=${this.state.apiKey}&q=${
-              this.state.searchText
-            }&image_type=photo&per_page=${this.state.amount}&safesearch=true`
+          `${this.state.apiUrl}/?key=${this.state.apiKey}&q=${this.state.searchText}&image_type=photo&per_page=${this.state.amount}&safesearch=true`
         )
         .then(res => this.setState({ images: res.data.hits }))
         .catch(err => console.log(err));
     });
   };
 
-  onAmountChange = (e, index, value) => this.setState({ amount: value});
+  onAmountChange = (e, index, value) => this.setState({ amount: value });
 
   render() {
     console.log(this.state.images);
@@ -53,6 +52,9 @@ class Search extends Component {
           <MenuItem value={50} primaryText="50" />
         </SelectField>
         <br />
+        {this.state.images.length > 0 ? (
+          <ImageResults images={this.state.images} />
+        ) : null}
       </div>
     );
   }
