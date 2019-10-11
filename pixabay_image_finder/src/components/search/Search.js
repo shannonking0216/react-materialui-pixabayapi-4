ebf3ae31363296aa28aa41b201d48c8767168c3b
Shannon King <kingshannon01@gmail.com>
2019-10-11 15:31:58 -0700

    react rename-unsafe-lifecycles

--- pixabay_image_finder/src/components/search/Search.js
+++ pixabay_image_finder/src/components/search/Search.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import TextField from "material-ui/TextField";
 import SelectField from "material-ui/SelectField";
 import MenuItem from "material-ui/MenuItem";
-import axios from 'axios';
+import axios from "axios";
 
 class Search extends Component {
   state = {
@@ -13,13 +13,23 @@ class Search extends Component {
     images: []
   };
 
-  onTextChange = (e) => {
-      this.setState({ [e.target.name]: e.target.value }, () => {   
-        axios.get(`${this.state.apiUrl}/?key=${this.state.apiKey}`)
-      });
+  onTextChange = e => {
+    this.setState({ [e.target.name]: e.target.value }, () => {
+      axios
+        .get(
+          `${this.state.apiUrl}/?key=${this.state.apiKey}&q=${
+              this.state.searchText
+            }&image_type=photo&per_page=${this.state.amount}&safesearch=true`
+        )
+        .then(res => this.setState({ images: res.data.hits }))
+        .catch(err => console.log(err));
+    });
   };
 
+  onAmountChange = (e, index, value) => this.setState({ amount: value});
+
   render() {
+    console.log(this.state.images);
     return (
       <div>
         <TextField
@@ -36,9 +46,9 @@ class Search extends Component {
           value={this.state.amount}
           onChange={this.onAmountChange}
         >
-          <MenuItem value={5} primaryText="5" />  
+          <MenuItem value={5} primaryText="5" />
           <MenuItem value={10} primaryText="10" />
-          <MenuItem value={15} primaryText="15" />  
+          <MenuItem value={15} primaryText="15" />
           <MenuItem value={30} primaryText="30" />
           <MenuItem value={50} primaryText="50" />
         </SelectField>
